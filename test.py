9e622f8b9c234e4d14516a5d6c2aaee45c5afdf8
Ryan Moore <ryan@geekportfolio.com>
2015-06-29 15:33:37 -0700

    slic optimized for sample image

--- test.py
+++ test.py
@@ -1,42 +1,60 @@
 from scipy import ndimage
-from skimage import data, io, filters
+from skimage import io as img_io
+from skimage.exposure import equalize_hist
+from skimage.transform import rescale
+from skimage.morphology import disk
 from skimage.color import label2rgb
 from skimage.segmentation import felzenszwalb, slic
 from matplotlib import pyplot as plt
 
 
-def segment_slic(fig, image):
-    segmented = slic(image, n_segments=4, convert2lab=True, enforce_connectivity=True)
-
-#    segmented = ndimage.binary_fill_holes(segmented - 1)
-    labels, _ = ndimage.label(segmented)
+def fig_label_segments(fig, image, segments, label):
+    labels, _ = ndimage.label(segments)
     image_label_overlay=label2rgb(labels, image=image)
-    fig.set_title('slic')
+    fig.set_title(label)
     fig.axis('off')
     fig.imshow(image_label_overlay)
-#    io.show()
 
-def segment_fz(fig, image):
-    segmented = felzenszwalb(image, scale=100, sigma=0.5, min_size=30)
-#    segmented = felzenszwalb(image)
+def segment_slic(fig, image):
+    segmented = slic(image, n_segments=6, convert2lab=True, enforce_connectivity=True)
 
     segmented = ndimage.binary_fill_holes(segmented - 1)
-    labels, _ = ndimage.label(segmented)
-    image_label_overlay=label2rgb(labels, image=image)
-    fig.set_title('felzenszwalb')
-    fig.axis('off')
-    fig.imshow(image_label_overlay)
-#    io.imshow(image_label_overlay)
-#    io.show()
+    fig_label_segments(fig, image, segmented, 'slic')
 
-input_image = data.astronaut()[::2, ::2]
+def segment_fz(fig, image):
+    segmented = felzenszwalb(image)
+    segmented = felzenszwalb(image, scale=50, sigma=0.5, min_size=30)
 
-fig, (ax1, ax2) = plt.subplots(1,2, figsize=(6,3))
+    segmented = ndimage.binary_fill_holes(segmented - 1)
+    fig_label_segments(fig, image, segmented, 'felzenszwalb')
 
-segment_fz(ax1, input_image)
-segment_slic(ax2, input_image)
+def get_input_image(fname):
 
-#margins = dict(hspace=0.01, wspace=0.01, top=1, bottom=0, left=0, right=1)
-#fig.subplots_adjust(**margins)
+    # read image and downsample by 2x
+    #input_image = img_io.imread('input_samples/27845089.jpg')[::2, ::2]
+
+    # don't downsample
+    input_image = img_io.imread(fname)
+
+    # select medians to filter outlier
+    input_image = ndimage.median_filter(input_image, 3)
+
+    # normalize / expand color range to maximize contrast
+    input_image = equalize_hist(input_image)
+
+
+    # scale image up by 2x for better visualization
+    input_image = rescale(input_image, 2)
+
+#    img_io.imshow(input_image)
+#    img_io.show()
+
+    return input_image
+
+image = get_input_image('input_samples/27845089.jpg')
+fig, (ax1, ax2) = plt.subplots(1,2, figsize=(12,6))
+
+segment_fz(ax1, image)
+segment_slic(ax2, image)
 
 plt.show()
