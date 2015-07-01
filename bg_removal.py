5325e5626986ae8b3306c290dc7c846eca449c81
Ryan Moore <ryan@geekportfolio.com>
2015-07-01 12:43:25 -0700

    final(ish) version for demo day

--- bg_removal.py
+++ bg_removal.py
@@ -1,4 +1,5 @@
 from glob import iglob
+from PIL import Image
 import numpy as np
 import re
 from matplotlib import pyplot as plt
@@ -38,7 +39,9 @@ def fig_label_segments(fig, image, segments, label):
     image_label_overlay = ~img_as_ubyte(image_label_overlay)
     fig.set_title(label)
     fig.axis('off')
-    fig.imshow(image_label_overlay)
+    cropped = crop_image_bounding(image_label_overlay)
+    cropped = crop_image_percent(cropped, 66)
+    fig.imshow(cropped)
 
 
 def show_entropy(fig, image):
@@ -76,7 +79,26 @@ def load_mask(fig, fn):
     image = get_input_image(fn)
     fig.set_title('old mask')
     fig.axis('off')
-    fig.imshow(image)
+    cropped = crop_image_bounding(image)
+    cropped = crop_image_percent(cropped, 66)
+    fig.imshow(cropped)
+
+# crop image to perc% of original size
+# so "66" will take a 600x600 image down to its centermost 396px
+def crop_image_percent(img, perc):
+    h, w, _ = img.shape
+    true_perc = 1 - (perc / 100.0)
+    crop_w = int(w * true_perc) / 2
+    crop_h = int(h * true_perc) / 2
+    return img[crop_h:(h-crop_h), crop_w:(w-crop_w)]
+
+def crop_image_bounding(img):
+    img = ~img_as_ubyte(img)
+    pil_img = Image.fromarray(img)
+    pil_img = pil_img.crop(pil_img.getbbox())
+    img = ~img_as_ubyte(np.asarray(pil_img))
+    return img
+
 
 # __main__
 fig_count = 3
@@ -87,11 +109,12 @@ for fn in iglob('input_samples/*.jpg'):
     outname = nr.sub(r'output_samples/\1.png', fn)
 
     fig, (ax0, ax1, ax2) = plt.subplots(1, fig_count, figsize=(fig_count * fig_scale_factor, fig_scale_factor))
+    fig.set_facecolor('#dddddd')
     image = show_orig(ax0, fn)
     load_mask(ax1, mask_fn)
     show_entropy(ax2, image)
 
     print("Saving %s to filename %s" % (fn, outname))
-    plt.savefig(outname)
+    plt.savefig(outname, facecolor=fig.get_facecolor())
     plt.close()
 
